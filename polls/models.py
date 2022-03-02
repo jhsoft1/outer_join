c5aa643be8f7b36ad7d5a79c8b16a26e0a59c852
jhsoft1 <jens@petja.de>
2022-03-02 20:48:39 +0100

    2

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.db import models
 from django.db.models import UniqueConstraint
 from django.urls import reverse
@@ -8,15 +9,27 @@ class Voter(models.Model):
 
     def __str__(self):
         return self.name
+
     def get_absolute_url(self):
         """Returns the url to access a particular author instance."""
         return reverse('voter-detail', args=[str(self.pk)])
 
 
+class Year(models.Model):
+    vote_year = models.IntegerField(primary_key=True)
+
+    def __str__(self):
+        return str(self.vote_year)
+
+    def get_absolute_url(self):
+        """Returns the url to access a particular author instance."""
+        return reverse('year-detail', args=[str(self.pk)])
+
+
 class Vote(models.Model):
     value = models.CharField(max_length=100)
-    voter = models.ForeignKey(Voter, on_delete=models.CASCADE)
-    year = models.PositiveIntegerField()
+    voter = models.ForeignKey(User, on_delete=models.CASCADE)
+    year = models.ForeignKey(Year, on_delete=models.CASCADE)
 
     class Meta:
         constraints = [UniqueConstraint(fields=['voter', 'year'], name='voter_year')]
--- polls/views.py
+++ polls/views.py
@@ -3,50 +3,56 @@ from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView
 
-from polls.models import Voter, Vote
+from polls.models import Voter, Vote, Year
 
 
-class VoterListView(ListView):
-    model = Voter
+class YearListView(ListView):
+    model = Year
 
 
-class VoterCreate(CreateView):
-    model = Voter
+class YearCreate(CreateView):
+    model = Year
     fields = '__all__'
 
 
-class VoterDetailView(DetailView):
-    model = Voter
+class YearDetailView(DetailView):
+    model = Year
 
 
-class VoterUpdate(UpdateView):
-    model = Voter
+class YearUpdate(UpdateView):
+    model = Year
     fields = '__all__'
 
 
-class VoterDelete(DeleteView):
-    model = Voter
-    success_url = reverse_lazy('voters')
+class YearDelete(DeleteView):
+    model = Year
+    success_url = reverse_lazy('years')
 
 
 class VoteListView(ListView):
     model = Vote
 
 
-class VoterOuterListView(ListView):
-    model = Voter
+class YearOuterListView(ListView):
+    model = Year
 
     # queryset = Voter.objects.annotate(value=F('vote__value'), year=F('vote__year')).values('name', 'value', 'year')
     # queryset = Voter.objects.annotate(value=F('vote__value'), year=F('vote__year')).values().filter(name=user)
     def get_queryset(self):
         # return Voter.objects.annotate(value=F('vote__value'), year=F('vote__year')).values().filter(name=self.request.user)
-        return Voter.objects.annotate(votes2020=FilteredRelation(
-            'vote', condition=Q(vote__year=2020))).values('name', 'votes2020__value', 'votes2020__year')
+        # return Voter.objects.annotate(votes2020=FilteredRelation(
+        #     'vote', condition=Q(vote__year=2020))).values('name', 'votes2020__value', 'votes2020__year')
+        return Year.objects.annotate(my_votes=FilteredRelation(
+            'vote', condition=Q(vote__voter=self.request.user))).values('vote_year', 'my_votes__value', 'my_votes__year')
 
 
 class VoteCreate(CreateView):
     model = Vote
-    fields = '__all__'
+    fields = ['value', 'year']
+
+    def form_valid(self, form):
+        form.instance.voter = self.request.user
+        return super().form_valid(form)
 
 
 class VoteDetailView(DetailView):
@@ -67,10 +73,10 @@ def index(request):
     """View function for home page of site."""
 
     # Generate counts of some main objects
-    num_voters = Voter.objects.count()
+    num_years = Year.objects.count()
     num_votes = Vote.objects.all().count()
     if request.user.is_authenticated:
-        num_my_votes = Vote.objects.filter(voter__name=request.user).count()
+        num_my_votes = Vote.objects.filter(voter=request.user).count()
     else:
         num_my_votes = 'We give whisky only to our friends :)'
 
@@ -78,7 +84,7 @@ def index(request):
     num_visits = request.session.get('num_visits', 0)
     request.session['num_visits'] = num_visits + 1
     context = {
-        'num_voters': num_voters,
+        'num_years': num_years,
         'num_votes': num_votes,
         'num_visits': num_visits,
         'num_my_votes': num_my_votes,
